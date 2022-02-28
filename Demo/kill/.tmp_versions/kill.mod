eeacf1b1c95c63de10f7117e2fd71889a5e7045a
wudi <nikoladi@163.com>
2022-02-28 15:10:20 +0800

    makefile bug

---
+++ Demo/kill/.tmp_versions/kill.mod
@@ -0,0 +1,2 @@
+/root/glx/MyToolsCode/Demo/kill/kill.ko
+/root/glx/MyToolsCode/Demo/kill/kill.o
--- Demo/kill/kill.c
+++ Demo/kill/kill.c
@@ -6,25 +6,13 @@ static int pid = -1;
 module_param(pid, int, S_IRUGO); 
 static int killd_init(void) 
 { 
-    struct task_struct * p; 
-    printk(KERN_ALERT "killd: force D status process to death/n"); 
-    printk(KERN_ALERT "killd: pid=%d/n", pid); 
-    //read_lock(&tasklist_lock); 
-    for_each_process(p){ 
-        if(p->pid == pid){ 
-            printk("killd: found/n"); 
-            set_task_state(p, TASK_STOPPED); 
-            printk(KERN_ALERT "killd: aha, dead already/n"); 
-            return 0; 
-        } 
-    } 
-    printk("not found"); 
-    //read_unlock(&tasklist_lock); 
+    printk("hello\r\n");
+    printk("S_IRUGO--%d\r\n",S_IRUGO); 
     return 0; 
 } 
 static void killd_exit(void) 
 { 
-    printk(KERN_ALERT "killd: bye/n"); 
+    printk("killd: bye\r\n"); 
 } 
 module_init(killd_init); 
 module_exit(killd_exit); 
